89f8bed8e0260d094ce8bc5e6dca3ed05dbd3493
Krishnakumar-58 <Krishnakumar.zamania@gmail.com>
2026-08-19 15:32:11 +0530

    day1done

---
+++ firstapp/src/App.jsx
@@ -0,0 +1,15 @@
+import About from './About';
+import './App.css'
+import Home from './Home'
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+
+  return (
+    <>
+     <Home/>
+     <About/>
+    </>
+  )
+}
+
+export default App
